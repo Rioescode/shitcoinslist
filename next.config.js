4fe23941d416d0b8f09c0284ef38450e23adf130
Yeimy Sarceno <esckoofficial@gmail.com>
2024-11-09 14:42:21 -0500

    Initial commit: ShitcoinsList.com

--- next.config.js
+++ next.config.js
@@ -1,14 +1,31 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      // NextJS <Image> component needs to whitelist domains for src={}
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "images.unsplash.com",
-      "logos-world.net",
-    ],
-  },
+    images: {
+        domains: [
+            's2.coinmarketcap.com',  // For CoinMarketCap logos
+            'assets.coingecko.com',  // Backup source for logos
+            'localhost'
+        ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's2.coinmarketcap.com',
+                pathname: '/static/img/coins/**',
+            }
+        ]
+    },
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type' }
+                ]
+            }
+        ];
+    }
 };
 
 module.exports = nextConfig;
